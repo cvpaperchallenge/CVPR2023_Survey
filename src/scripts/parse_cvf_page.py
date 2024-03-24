f4bcef9703fd583343433f718e21f460563e61a7
botany <yoshikikubotani.lab@gmail.com>
2024-03-24 04:49:38 +0000

    :sparkles: Use pydantic v2 notations

--- src/scripts/parse_cvf_page.py
+++ src/scripts/parse_cvf_page.py
@@ -8,12 +8,19 @@ import argparse
 import json
 import logging
 import pathlib
-from typing import Final
+from typing import Final, Any
+from pydantic_core import Url
 
 from src.parser import get_paper_page_urls, parse_paper_page
 
 logger: Final = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
+
+def url_serializer_for_json_dump(object: Any) -> str:
+    if isinstance(object, Url):
+        return str(object)
+    raise TypeError(f'Object of type {object.__class__.__name__} is not JSON serializable')
+
 
 def extract_paper_info(output_dir: pathlib.Path, conference: str, year: int) -> None:
     """
@@ -31,12 +38,12 @@ def extract_paper_info(output_dir: pathlib.Path, conference: str, year: int) -> 
     for i, url in enumerate(urls):
         logger.info(f"Processing {i+1}/{len(urls)}: {url}")
         paper = parse_paper_page(url)
-        papers.append(paper.dict())
+        papers.append(paper.model_dump())
 
     output_dir.mkdir(parents=True, exist_ok=True)
     output_path: Final = output_dir / f"{conference}{year}_papers.json"
     with output_path.open("w") as f:
-        json.dump(papers, f, indent=4)
+        json.dump(papers, f, indent=4, default=url_serializer_for_json_dump)
 
     logger.info(f"Successfully parsed {len(papers)} papers.")
 
