8e4b275db63eef6d0233acf99556d0df4b652eb3
botany <yoshikikubotani.lab@gmail.com>
2024-03-24 23:49:56 +0900

    :art: Rename a variable

--- src/scripts/download_papers.py
+++ src/scripts/download_papers.py
@@ -42,8 +42,8 @@ def download_paper_pdfs(output_root_dir: pathlib.Path, paper_info_path: pathlib.
 
         # filename is like: <family_name>_<paper_title>_<conference_name>_<year>_paper.pdf
         filename = str(paper.pdf).split("/")[-1]
-        prefix, conference_name, year, _ = filename.rsplit("_", 3)
-        family_name, paper_title = prefix.split("_", 1)
+        core, conference_name, year, _ = filename.rsplit("_", 3)
+        family_name, paper_title = core.split("_", 1)
         directory_path = output_root_dir / pathlib.Path(conference_name + year) / pathlib.Path(paper_title)
         file_path = directory_path / filename
 
