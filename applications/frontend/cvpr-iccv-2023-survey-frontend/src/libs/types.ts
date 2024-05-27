83d0e66badb0c9393a40571e5c9a01f355778367
botany <yoshikikubotani.lab@gmail.com>
2024-05-28 01:00:57 +0900

    :sparkles: Add paper details interface

--- applications/frontend/cvpr-iccv-2023-survey-frontend/src/libs/types.ts
+++ applications/frontend/cvpr-iccv-2023-survey-frontend/src/libs/types.ts
@@ -11,3 +11,24 @@ export interface Paper {
   cvf: string;
   pdf: string;
 }
+
+export interface PaperInfo {
+  title: string;
+  abstract: string;
+  authors: string[];
+  cvfLink: string;
+  pdfLink: string;
+}
+
+export interface Summary {
+  outline: string;
+  contribution: string;
+  method: string;
+  evaluation: string;
+  discussion: string;
+}
+
+export interface PaperDetails {
+  paperInfo: PaperInfo;
+  summary: Summary;
+}
