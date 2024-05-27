fcc4f86aff51682b26c44d3b2e5f51037f01a531
botany <yoshikikubotani.lab@gmail.com>
2024-05-28 01:01:57 +0900

    :sparkles: Add fetch for paper details

--- applications/frontend/cvpr-iccv-2023-survey-frontend/src/libs/fetch.tsx
+++ applications/frontend/cvpr-iccv-2023-survey-frontend/src/libs/fetch.tsx
@@ -1,4 +1,4 @@
-import { Paper, FetchResult } from './types';
+import { Paper, PaperDetails, FetchResult } from './types';
 
 async function fetchFromAPI<T>(url: string, options?: RequestInit): Promise<FetchResult<T>> {
   try {
@@ -34,4 +34,9 @@ export async function searchPapers(query: string, conference: string): Promise<F
     body: JSON.stringify({ query }),
   };
   return fetchFromAPI<Paper[]>(url, options);
+}
+
+export async function getPaperDetails(conference: string, id: string): Promise<FetchResult<PaperDetails>> {
+  const url = `${process.env.NEXT_PUBLIC_BACKEND_API_URL}/${conference}/${id}`;
+  return fetchFromAPI<PaperDetails>(url);
 }
