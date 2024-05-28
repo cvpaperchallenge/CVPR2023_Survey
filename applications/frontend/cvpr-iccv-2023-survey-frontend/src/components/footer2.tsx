3999eb6244766d11e6ddede5c6c078874494eae6
botany <yoshikikubotani.lab@gmail.com>
2024-05-29 00:45:11 +0900

    :sparkles: Add another pattern of header/footer

---
+++ applications/frontend/cvpr-iccv-2023-survey-frontend/src/components/footer2.tsx
@@ -0,0 +1,85 @@
+'use client'
+import { MagnifyingGlassIcon, DotsHorizontalIcon, GitHubLogoIcon, TwitterLogoIcon, SunIcon, DesktopIcon, MoonIcon } from '@radix-ui/react-icons'
+import {Link, Flex, IconButton, Text, SegmentedControl, Separator} from '@radix-ui/themes'
+
+export default function Footer(){
+  const year = new Date().getFullYear()
+
+  return (
+    <section
+      style={{
+        background: 'var(--accent-2)',
+    }}>
+      <Flex
+        direction="row"
+        justify="between"
+        align="center"
+        style={{
+          flexShrink: 0,
+          padding: "30px 50px",
+          margin: "0 auto",
+          width: "1100px",
+          maxWidth: "100%",
+      }}>
+        <Flex direction="row" align="center" gap="5">
+          <Flex direction="column" align="start" gap="2" >
+            <img
+              src="/cc_logo_2white.png"
+              alt="logo"
+              style={{
+                objectFit: 'cover',
+                width: '240px',
+                height: '23px',
+              }}
+            />
+            <Text color="gray" size="2" style={{paddingLeft: "5px"}}>&copy; 2015-{year}</Text>
+          </Flex>
+          <Separator orientation="vertical" size="2"/>
+          <Flex direction="row" align="center" gap="5">
+            <Text color="gray" size="2">
+              Supported by SSII 2024
+            </Text>
+          </Flex>
+        </Flex>
+        <Flex direction="row" align="center" gap="3">
+          <Flex direction="column" align="start" gap="2">
+            <Flex direction="column" align="start" gap="2">
+              <Text color="gray" size="2">
+                Developed by<br/>
+              </Text>
+              <Flex direction="column" align="start" gap="1">
+                <Link href="https://github.com/YoshikiKubotani">
+                  <Flex direction="row" justify="center" gap="1">
+                    <GitHubLogoIcon />
+                    <Text color="gray" size="1" weight="light">Yoshiki Kubotani</Text>
+                  </Flex>
+                </Link>
+                <Link href="https://github.com/gatheluck">
+                  <Flex direction="row" justify="center" gap="1">
+                    <GitHubLogoIcon />
+                    <Text color="gray" size="1" weight="light">Yoshihiro Fukuhara</Text>
+                  </Flex>
+                </Link>
+                <Link href="https://github.com/Hina39">
+                  <Flex direction="row" justify="center" gap="1">
+                    <GitHubLogoIcon />
+                    <Text color="gray" size="1" weight="light">Hina Otake</Text>
+                  </Flex>
+                </Link>
+              </Flex>
+            </Flex>
+          </Flex>
+          <img
+            src="/forward-propergation-chan4.png"
+            alt="jundenpa_chan"
+            style={{
+              objectFit: 'cover',
+              width: '40px',
+              height: '40px',
+            }}
+          />
+        </Flex>
+      </Flex>
+    </section>
+  )
+}
