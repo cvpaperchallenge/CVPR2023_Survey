9bd6b8227eb9bf136166ee364908fcca3952933a
botany <yoshikikubotani.lab@gmail.com>
2024-06-04 18:10:58 +0900

    :sparkles: Add border and background colors for header/footer

--- applications/frontend/my-app/src/app/(dashboard)/layout.tsx
+++ applications/frontend/my-app/src/app/(dashboard)/layout.tsx
@@ -9,11 +9,11 @@ export default function PaperListLayout({
 }) {
   return (
     <div className="flex min-h-screen w-screen flex-col items-center gap-8 ">
-      <div className="h-24 w-full flex-none">
+      <div className="h-24 w-full flex-none border-b dark:bg-card bg-teal-100">
         <Header />
       </div>
       <div className="flex-1 grow">{children}</div>
-      <div className="h-44 w-full flex-none">
+      <div className="h-44 w-full flex-none border-t dark:bg-card bg-teal-100">
         <Footer />
       </div>
     </div>
