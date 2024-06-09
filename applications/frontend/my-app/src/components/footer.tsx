e8c9bd6bf28d3766107994055b9e4a13b9f07e40
botany <yoshikikubotani.lab@gmail.com>
2024-06-09 17:09:27 +0900

    :art: Add transition delay to links

--- applications/frontend/my-app/src/components/footer.tsx
+++ applications/frontend/my-app/src/components/footer.tsx
@@ -65,7 +65,7 @@ export default function Footer() {
           Supported by{' '}
           <span className="inline">
             <a
-              className="text-primary hover:border-b-2 hover:bg-accent hover:text-accent-foreground active:border-b-2 active:bg-primary active:text-primary-foreground"
+              className="text-primary hover:border-b-2 hover:bg-accent hover:text-accent-foreground active:border-b-2 active:bg-primary active:text-primary-foreground transition-colors"
               href="https://confit.atlas.jp/guide/event/ssii2024/top?lang=ja"
               target="_blank"
             >
@@ -93,7 +93,7 @@ export default function Footer() {
           </div>
           <div className="flex min-w-[130px] flex-col items-start gap-1">
             <a
-              className="p-0.5 hover:bg-[var(--teal-4)] hover:text-foreground active:bg-primary active:text-primary-foreground"
+              className="p-0.5 hover:bg-[var(--teal-4)] hover:text-foreground active:bg-primary active:text-primary-foreground transition-colors"
               href="https://github.com/YoshikiKubotani"
               target="_blank"
             >
@@ -105,7 +105,7 @@ export default function Footer() {
               </div>
             </a>
             <a
-              className="p-0.5 hover:bg-[var(--teal-4)] hover:text-foreground active:bg-primary active:text-primary-foreground"
+              className="p-0.5 hover:bg-[var(--teal-4)] hover:text-foreground active:bg-primary active:text-primary-foreground transition-colors"
               href="https://github.com/gatheluck"
               target="_blank"
             >
@@ -117,7 +117,7 @@ export default function Footer() {
               </div>
             </a>
             <a
-              className="p-0.5 hover:bg-[var(--teal-4)] hover:text-foreground active:bg-primary active:text-primary-foreground"
+              className="p-0.5 hover:bg-[var(--teal-4)] hover:text-foreground active:bg-primary active:text-primary-foreground transition-colors"
               href="https://github.com/Hina39"
               target="_blank"
             >
