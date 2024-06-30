cc48ef9db8f44dca085d38961c2a8935b5931187
botany <yoshikikubotani.lab@gmail.com>
2024-06-30 19:40:53 +0900

    :art: Separate the cell content to react components

--- applications/frontend/my-app/src/app/(dashboard)/list/columns.tsx
+++ applications/frontend/my-app/src/app/(dashboard)/list/columns.tsx
@@ -1,4 +1,4 @@
-import { ColumnDef } from '@tanstack/react-table'
+import { ColumnDef, Row } from '@tanstack/react-table'
 import { useRouter } from 'next/navigation'
 import { LuArrowUpDown } from 'react-icons/lu'
 import { RxFile, RxGlobe } from 'react-icons/rx'
@@ -7,6 +7,78 @@ import { Button } from '@/components/ui/button'
 import { ScrollArea } from '@/components/ui/scroll-area'
 
 import { PaperInfo } from '@/lib/types'
+
+const TitleCell = ({ row }: { row: Row<PaperInfo> }) => {
+  const router = useRouter()
+  const authors: string[] = row.getValue('authors')
+  return (
+    <div className="flex flex-col justify-start">
+      <a
+        className="cursor-pointer rounded px-2 py-1 transition-colors hover:bg-accent hover:text-accent-foreground active:bg-primary active:text-primary-foreground"
+        onClick={() => router.push(`/details?id=${row.index}`)}
+      >
+        <div className="font-semibold">{row.getValue('title')}</div>
+      </a>
+      <ScrollArea className="mt-1 h-10">
+        <div className="px-2 pb-2 text-sm text-muted-foreground ">
+          {authors.join(', ')}
+        </div>
+      </ScrollArea>
+    </div>
+  )
+}
+
+const AuthorsCell = ({ row }: { row: Row<PaperInfo> }) => {
+  const authors: string[] = row.getValue('authors')
+  return <div>{authors.join(', ')}</div>
+}
+
+const LinkCell = ({ row }: { row: Row<PaperInfo> }) => {
+  return (
+    <div className="flex flex-col items-center gap-1.5">
+      <a href={row.original.cvfLink} rel="noreferrer" target="_blank">
+        <Button
+          className="
+            h-6
+            gap-1
+            border-[var(--jade-6)]
+            bg-[var(--jade-4)]
+            px-2
+            text-xs
+            text-[var(--jade-11)]
+            hover:bg-[var(--jade-5)]
+            hover:text-[var(--jade-12)]
+          "
+          variant="outline"
+        >
+          <RxGlobe /> CVF
+        </Button>
+      </a>
+      <a href={row.original.pdfLink} rel="noreferrer" target="_blank">
+        <Button
+          className="
+            h-6
+            gap-1
+            border-[var(--red-6)]
+            bg-[var(--red-3)]
+            px-2
+            text-xs
+            text-[var(--red-11)]
+            hover:bg-[var(--red-4)]
+            hover:text-[var(--red-12)]
+          "
+          variant="outline"
+        >
+          <RxFile /> PDF
+        </Button>
+      </a>
+    </div>
+  )
+}
+
+const ConferenceCell = ({ row }: { row: Row<PaperInfo> }) => {
+  return <div className="text-center">{row.original.conference}</div>
+}
 
 export const columns: ColumnDef<PaperInfo>[] = [
   {
@@ -22,97 +94,26 @@ export const columns: ColumnDef<PaperInfo>[] = [
         </Button>
       )
     },
-    cell: ({ row }) => {
-      const router = useRouter()
-      const authors: string[] = row.getValue('authors')
-      return (
-        <div className="flex flex-col justify-start">
-          <a
-            className=" cursor-pointer rounded px-2 py-1 transition-colors hover:bg-accent hover:text-accent-foreground active:bg-primary active:text-primary-foreground"
-            onClick={() => router.push(`/details?id=${row.index}`)}
-          >
-            <div className="font-semibold">{row.getValue('title')}</div>
-          </a>
-          <ScrollArea className="mt-1 h-10">
-            <div className="px-2 pb-2 text-sm text-muted-foreground ">
-              {authors.join(', ')}
-            </div>
-          </ScrollArea>
-        </div>
-      )
-    },
+    cell: TitleCell,
   },
   {
     accessorKey: 'authors',
     header: 'Authors',
-    cell: ({ row }) => {
-      const authors: string[] = row.getValue('authors')
-      return <div>{authors.join(', ')}</div>
-    },
+    cell: AuthorsCell,
     filterFn: (row, id, filterValue) => {
-      // console.log(`Filtering authors with value: ${filterValue}`)
-      // console.log(rows)
       const authorsArray = row.original.authors
-      // Return true if authorsArray contains any author that includes filterValue
       return authorsArray.some((author) => (filterValue as string[]).includes(author))
-      // return rows.filter((row) => {
-      //   const authors = row.values[id] as string[]
-      //   return authors.some((author) => author.includes(filterValue))
-      // })
     },
   },
   {
     accessorKey: 'link',
     header: () => <div className="text-center">Link</div>,
-    cell: ({ row }) => {
-      return (
-        <div className="flex flex-col items-center gap-1.5">
-          <a href={row.original.cvfLink} rel="noreferrer" target="_blank">
-            <Button
-              className="
-              h-6
-              gap-1
-              border-[var(--jade-6)]
-              bg-[var(--jade-4)]
-              px-2
-              text-xs
-              text-[var(--jade-11)]
-              hover:bg-[var(--jade-5)]
-              hover:text-[var(--jade-12)]
-            "
-              variant="outline"
-            >
-              <RxGlobe /> CVF
-            </Button>
-          </a>
-          <a href={row.original.pdfLink} rel="noreferrer" target="_blank">
-            <Button
-              className="
-                h-6
-                gap-1
-                border-[var(--red-6)]
-                bg-[var(--red-3)]
-                px-2
-                text-xs
-                text-[var(--red-11)]
-                hover:bg-[var(--red-4)]
-                hover:text-[var(--red-12)]
-            "
-              variant="outline"
-            >
-              <RxFile /> PDF
-            </Button>
-          </a>
-        </div>
-      )
-    },
+    cell: LinkCell,
   },
   {
     accessorKey: 'conference',
     header: 'Conference',
-    cell: ({ row }) => {
-      return <div className="text-center">{row.original.conference}</div>
-    },
+    cell: ConferenceCell,
     filterFn: (row, id, filterValue) => {
       return filterValue.includes(row.getValue(id))
     },
