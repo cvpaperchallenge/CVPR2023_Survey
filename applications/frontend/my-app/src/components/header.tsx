00aa27933f92dc99c70c82799acc4597b9dd7da5
botany <yoshikikubotani.lab@gmail.com>
2024-07-01 15:21:47 +0900

    :sparkles: Add spinner to theme toggle switch

--- applications/frontend/my-app/src/components/header.tsx
+++ applications/frontend/my-app/src/components/header.tsx
@@ -1,12 +1,14 @@
 'use client'
 import Image from 'next/image'
 import { useTheme } from 'next-themes'
+import { useState, useEffect } from 'react'
 import { PiXLogo } from 'react-icons/pi'
 import { RxGithubLogo, RxMoon, RxSun } from 'react-icons/rx'
 
 import { FeedbackDialog } from '@/components/feedback-dialog'
 import { Button } from '@/components/ui/button'
 import { Separator } from '@/components/ui/separator'
+import { Spinner } from '@/components/ui/spinner'
 import { Switch } from '@/components/ui/switch'
 
 import blackLogo from '../../public/cc_logo_2.png'
@@ -14,6 +16,15 @@ import whiteLogo from '../../public/cc_logo_2white.png'
 
 export default function Header() {
   const { theme, setTheme } = useTheme()
+  const [isDarkMode, setIsDarkMode] = useState(false)
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    if (theme) {
+      setIsDarkMode(theme === 'dark')
+      setIsLoading(false)
+    }
+  }, [theme])
   return (
     <div className="flex h-full items-center justify-between px-4 py-2.5 text-primary">
       <a href="https://xpaperchallenge.org/cv/" target="_blank">
@@ -48,33 +59,39 @@ export default function Header() {
           min-[501px]:flex-row min-[501px]:max-[600px]:gap-4 min-[601px]:gap-6
         "
         >
-          <div className="flex items-center space-x-1 max-[501px]:hidden min-[461px]:space-x-2">
-            <RxSun
-              className="
-              size-3
-              min-[461px]:max-[600px]:size-3.5
-              min-[601px]:size-4
-            "
-            />
-            <Switch
-              checked={theme === 'dark'}
-              className="
-              h-4 w-8 min-[461px]:max-[600px]:h-5
-              min-[461px]:max-[600px]:w-10 min-[601px]:h-6 min-[601px]:w-11
-            "
-              id="airplane-mode"
-              onCheckedChange={(checked) => {
-                setTheme(checked ? 'dark' : 'light')
-              }}
-            />
-            <RxMoon
-              className="
-              size-3
-              min-[461px]:max-[600px]:size-3.5
-              min-[601px]:size-4
-            "
-            />
-          </div>
+          {isLoading ?
+            <div className="flex w-24 flex-row justify-center">
+              <Spinner size="small" />
+            </div> :
+            <div className="flex items-center space-x-1 max-[501px]:hidden min-[461px]:space-x-2">
+              <RxSun
+                className="
+                size-3
+                min-[461px]:max-[600px]:size-3.5
+                min-[601px]:size-4
+              "
+              />
+              <Switch
+                checked={isDarkMode}
+                className="
+                h-4 w-8 min-[461px]:max-[600px]:h-5
+                min-[461px]:max-[600px]:w-10 min-[601px]:h-6 min-[601px]:w-11
+              "
+                id="airplane-mode"
+                onCheckedChange={(checked) => {
+                  setTheme(checked ? 'dark' : 'light')
+                  setIsDarkMode(checked)
+                }}
+              />
+              <RxMoon
+                className="
+                size-3
+                min-[461px]:max-[600px]:size-3.5
+                min-[601px]:size-4
+              "
+              />
+            </div>
+          }
           <div
             className="
             flex flex-row items-center
